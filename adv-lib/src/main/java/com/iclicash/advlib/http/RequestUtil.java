7331f35193c8bc0730a0100d04662175c13b597f
Christain <495419499@qq.com>
2018-09-18 22:36:34 +0800

    线程池管理网络请求

--- adv-lib/src/main/java/com/iclicash/advlib/http/RequestUtil.java
+++ adv-lib/src/main/java/com/iclicash/advlib/http/RequestUtil.java
@@ -2,6 +2,8 @@ package com.iclicash.advlib.http;
 
 import android.text.TextUtils;
 
+import com.iclicash.advlib.imageLoader.ImageThreadPoolExecutor;
+
 import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
@@ -10,20 +12,21 @@ import java.util.List;
 import java.util.Map;
 
 
-public class RequestUtil{
-    private Thread mThread;
+public class RequestUtil {
+//    private Thread mThread;
+    private Runnable mRunnable;
 
     /**
      * 一般的get请求或post请求
      */
-    RequestUtil(String method,String url, Map<String, String> paramsMap, Map<String, String> headerMap, CallBackUtil callBack) {
-        switch (method){
+    RequestUtil(String method, String url, Map<String, String> paramsMap, Map<String, String> headerMap, CallBackUtil callBack) {
+        switch (method) {
             case "GET":
-            urlHttpGet(url,paramsMap,headerMap,callBack);
-            break;
+                urlHttpGet(url, paramsMap, headerMap, callBack);
+                break;
             case "POST":
-            urlHttpPost(url,paramsMap,null,headerMap,callBack);
-            break;
+                urlHttpPost(url, paramsMap, null, headerMap, callBack);
+                break;
         }
     }
 
@@ -31,85 +34,116 @@ public class RequestUtil{
      * post请求，传递json格式数据。
      */
     RequestUtil(String url, String jsonStr, Map<String, String> headerMap, CallBackUtil callBack) {
-        urlHttpPost(url,null,jsonStr,headerMap,callBack);
+        urlHttpPost(url, null, jsonStr, headerMap, callBack);
     }
 
     /**
      * 上传文件
      */
-    RequestUtil(String url, File file ,List<File> fileList,Map<String,File> fileMap,String fileKey,String fileType, Map<String,String> paramsMap, Map<String, String> headerMap,CallBackUtil callBack) {
-        urlHttpUploadFile(url,file,fileList,fileMap,fileKey,fileType,paramsMap,headerMap,callBack);
+    RequestUtil(String url, File file, List<File> fileList, Map<String, File> fileMap, String fileKey, String fileType, Map<String, String> paramsMap, Map<String, String> headerMap, CallBackUtil callBack) {
+        urlHttpUploadFile(url, file, fileList, fileMap, fileKey, fileType, paramsMap, headerMap, callBack);
     }
 
     /**
      * get请求
      */
     private void urlHttpGet(final String url, final Map<String, String> paramsMap, final Map<String, String> headerMap, final CallBackUtil callBack) {
-        mThread = new Thread(new Runnable() {
+//        mThread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                RealResponse response = new RealRequest().getData(getUrl(url, paramsMap), headerMap);
+//                if (response.code == HttpURLConnection.HTTP_OK) {
+//                    callBack.onSeccess(response);
+//                } else {
+//                    callBack.onError(response);
+//                }
+//            }
+//
+//        });
+        mRunnable = new Runnable() {
             @Override
             public void run() {
-                RealResponse response = new RealRequest().getData(getUrl(url,paramsMap),headerMap);
-                if(response.code == HttpURLConnection.HTTP_OK){
+                RealResponse response = new RealRequest().getData(getUrl(url, paramsMap), headerMap);
+                if (response.code == HttpURLConnection.HTTP_OK) {
                     callBack.onSeccess(response);
-                }else {
+                } else {
                     callBack.onError(response);
                 }
             }
-
-        });
+        };
     }
 
     /**
      * post请求
      */
     private void urlHttpPost(final String url, final Map<String, String> paramsMap, final String jsonStr, final Map<String, String> headerMap, final CallBackUtil callBack) {
-        mThread = new Thread(new Runnable() {
+//        mThread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                RealResponse response = new RealRequest().postData(url, getPostBody(paramsMap, jsonStr), getPostBodyType(paramsMap, jsonStr), headerMap);
+//                if (response.code == HttpURLConnection.HTTP_OK) {
+//                    callBack.onSeccess(response);
+//                } else {
+//                    callBack.onError(response);
+//                }
+//
+//            }
+//
+//        });
+        mRunnable = new Runnable() {
             @Override
             public void run() {
-                RealResponse response = new RealRequest().postData(url, getPostBody(paramsMap,jsonStr),getPostBodyType(paramsMap,jsonStr),headerMap);
-                if(response.code == HttpURLConnection.HTTP_OK){
+                RealResponse response = new RealRequest().postData(url, getPostBody(paramsMap, jsonStr), getPostBodyType(paramsMap, jsonStr), headerMap);
+                if (response.code == HttpURLConnection.HTTP_OK) {
                     callBack.onSeccess(response);
-                }else {
+                } else {
                     callBack.onError(response);
                 }
-
             }
-
-        });
-
+        };
     }
 
     /**
      * 上传文件
      */
-    private void urlHttpUploadFile(final String url, final File file , final List<File> fileList, final Map<String,File> fileMap, final String fileKey, final String fileType, final Map<String,String> paramsMap, final Map<String, String> headerMap, final CallBackUtil callBack) {
-        mThread = new Thread(new Runnable() {
+    private void urlHttpUploadFile(final String url, final File file, final List<File> fileList, final Map<String, File> fileMap, final String fileKey, final String fileType, final Map<String, String> paramsMap, final Map<String, String> headerMap, final CallBackUtil callBack) {
+//        mThread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                RealResponse response = null;
+//                response = new RealRequest().uploadFile(url, file,fileList,fileMap,fileKey,fileType,paramsMap,headerMap,callBack);
+//                if(response.code == HttpURLConnection.HTTP_OK){
+//                    callBack.onSeccess(response);
+//                }else {
+//                    callBack.onError(response);
+//                }
+//            }
+//
+//        });
+        mRunnable = new Runnable() {
             @Override
             public void run() {
                 RealResponse response = null;
-                response = new RealRequest().uploadFile(url, file,fileList,fileMap,fileKey,fileType,paramsMap,headerMap,callBack);
-                if(response.code == HttpURLConnection.HTTP_OK){
+                response = new RealRequest().uploadFile(url, file, fileList, fileMap, fileKey, fileType, paramsMap, headerMap, callBack);
+                if (response.code == HttpURLConnection.HTTP_OK) {
                     callBack.onSeccess(response);
-                }else {
+                } else {
                     callBack.onError(response);
                 }
             }
-
-        });
+        };
     }
-
-
 
     /**
      * get请求，将键值对凭接到url上
      */
-    private String getUrl(String path,Map<String, String> paramsMap) {
-        if(paramsMap != null){
-            path = path+"?";
-            for (String key: paramsMap.keySet()){
-                path = path + key+"="+paramsMap.get(key)+"&";
+    private String getUrl(String path, Map<String, String> paramsMap) {
+        if (paramsMap != null) {
+            path = path + "?";
+            for (String key : paramsMap.keySet()) {
+                path = path + key + "=" + paramsMap.get(key) + "&";
             }
-            path = path.substring(0,path.length()-1);
+            path = path.substring(0, path.length() - 1);
         }
         return path;
     }
@@ -117,20 +151,19 @@ public class RequestUtil{
     /**
      * 得到post请求的body
      */
-    private  String getPostBody(Map<String, String> params,String jsonStr) {//throws UnsupportedEncodingException {
-        if(params != null){
+    private String getPostBody(Map<String, String> params, String jsonStr) {//throws UnsupportedEncodingException {
+        if (params != null) {
             return getPostBodyFormParameMap(params);
-        }else if(!TextUtils.isEmpty(jsonStr)){
+        } else if (!TextUtils.isEmpty(jsonStr)) {
             return jsonStr;
         }
         return null;
     }
 
-
     /**
      * 根据键值对参数得到body
      */
-    private  String getPostBodyFormParameMap(Map<String, String> params) {//throws UnsupportedEncodingException {
+    private String getPostBodyFormParameMap(Map<String, String> params) {//throws UnsupportedEncodingException {
         StringBuilder result = new StringBuilder();
         boolean first = true;
         try {
@@ -154,27 +187,24 @@ public class RequestUtil{
      * 得到bodyType
      */
     private String getPostBodyType(Map<String, String> paramsMap, String jsonStr) {
-        if(paramsMap != null){
+        if (paramsMap != null) {
             //return "text/plain";不知为什么这儿总是报错。目前暂不设置(20170424)
             return null;
-        }else if(!TextUtils.isEmpty(jsonStr)){
+        } else if (!TextUtils.isEmpty(jsonStr)) {
             return "application/json;charset=utf-8";
         }
         return null;
     }
 
-
     /**
      * 开启子线程，调用run方法
      */
-    void execute(){
-        if(mThread != null){
-            mThread.start();
+    public void execute() {
+        if (mRunnable != null) {
+            ImageThreadPoolExecutor.getInstance().execute(mRunnable);
         }
+//        if(mThread != null){
+//            mThread.start();
+//        }
     }
-
-
-
-
-
 }
